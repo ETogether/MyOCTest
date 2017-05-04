023a49373dedac710e27b8aff048583defbbb03c
HeChangQing <r&d294@netvox.com.cn>
2017-05-04 14:43:54 +0800

    Initial Commit

---
+++ MyOCTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyOCTest
+//
+//  Created by netvox-ios1 on 2017/5/4.
+//  Copyright © 2017年 netvox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyOCTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyOCTest
+//
+//  Created by netvox-ios1 on 2017/5/4.
+//  Copyright © 2017年 netvox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
